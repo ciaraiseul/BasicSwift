9d9786a118842a54f199de5b3c71a38d7ecd14d9
ciaraiseul <139107560+ciaraiseul@users.noreply.github.com>
2024-08-01 10:13:57 +0700

    BVN

--- BasicSwift/main.swift
+++ BasicSwift/main.swift
@@ -1,70 +1,53 @@
-//
-//  main.swift
-//  BasicSwift
-//
-//  Created by Phạm Trường Giang on 31/07/2024.
-//
-
 import Foundation
 
-// Bài 1: cách 1
-func kiemTraN(n: Int){
-    if  n>=0 {
-        print("Yes")
-    } else{
-        print("No")
-    }
-    
-    if n%2==0{
-        print("Yes")
-    } else {
-        print("No")
-    }
-    
-    if n%5==0{
-        print("Yes")
-    } else {
-        print("No")
-    }
-}
-kiemTraN(n: 5)
+// Bài 1: Viết chương trình nhập vào số nguyên n. Hỏi:
+//        n có phải là số nguyên tố hay không?
 
-// Bài 1: cách 2
-print("Nhap tuoi của bạn: ")
-let age = Int(readLine() ?? "") ?? 0
-//if let age = ageStr {
-//    if let ageNumber = Int(age){
-//        print(age)
-//    }
-//}
-func kiemTran() {
-    print("Nhap n:", terminator: "")
+func kiemTraSNT(_ n: Int) -> Bool {
+    if n < 2 {
+        return false
+    }
+    var i = 2
+    let sqrtN = Int(sqrt(Double(n)))
+    while i <= sqrtN {
+        if n % i == 0 {
+            return false
+        }
+        i += 1
+    }
+    return true
+}
+
+print("Nhập vào số nguyên n: ", terminator: "")
     let n = Int(readLine() ?? "") ?? 0
-    var message: String = "So \(n) là:"
-    // kiem tra so am duong
-    if n>0{
-        message += "So duong, "
-    } else if n<0{
-        message += "So am, "
+    if kiemTraSNT(n) {
+        print("\(n) là số nguyên tố.")
     } else {
-        message += "So 0, Test"
+        print("\(n) không phải là số nguyên tố.")
+}
+//
+//  Bài 2: Tìm số n bé nhất sao cho n! lớn hơn một số m cho trước (m nhập từ bàn phím).
+
+
+func giaiThua(_ c: Int) -> Int {
+    if c == 0 {
+        return 1
     }
+    return c * giaiThua(c - 1)
+}
+
+func timSo(_ m: Int) -> Int {
+    var c = 1
+    while giaiThua(c) <= m {
+        c += 1
+    }
+    return c
 }
 
 
-
-
-func kiemTraTamGiac(a: Int, b: Int, c: Int) {
-    if a + b > c && a + c > b && b + c > a {
-        print("a, b, c là 3 cạnh của tam giác")
-        let p = (a + b + c) / 2
-        let s = sqrt(Double(p) * (Double(p) - Double(a)) * (Double(p) - Double(b)) * (Double(p) - Double(c)))
-        print("Diện tích của tam giác là: \(s)")
-    } else {
-        print("a, b, c không phải 3 cạnh của tam giác")
-    }
-}
-
-kiemTraTamGiac(a: 5, b: 6, c: 7)
-
+print("Nhập vào số m: ", terminator: "")
+    let m = Int(readLine() ?? "") ?? 0
+        let c = timSo(m)
+        print("Số n bé nhất sao cho n! > \(m) là \(c).")
+    
 
